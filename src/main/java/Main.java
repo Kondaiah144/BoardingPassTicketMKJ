8f32fb6e8eb13c5e5d1ebc6aec779b2b0021348f
MehmedVM <mehmedvm@gmail.com>
2022-05-04 16:46:18 -0400

    MM 5-4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        Date d1 = new Date();
+        System.out.println("Current date is " + d1);
+        //Date d2 = new Date(2323223232L);
+        //System.out.println("Date represented is "+ d2 );
+
+        Date d3 = new Date(2022,01, 01);
+        System.out.println(d3);
+
+        String sDate1="31/12/1998";
+        Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
+        System.out.println(sDate1+"\t"+date1);
+
         
     }
 }
---
+++ src/main/java/Ticket.java
@@ -0,0 +1,19 @@
+public class Ticket {
+    //Fields
+    String passengerName;
+    int passengerAge;
+    String passengerEmail;
+    String passengerGender;
+    String passengerPhoneNo;
+    String departureDate;
+    int boardingPassNo;
+    String originAirport;
+    String destinationAirport;
+
+    //Constructors
+
+    // Getters and Setters
+
+    //To String
+
+}
