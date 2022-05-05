6d855302622718a9afa050ec73cc90bafaeb409b
MehmedVM <mehmedvm@gmail.com>
2022-05-05 15:51:40 -0400

    5-5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,100 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws ParseException {
-        Date d1 = new Date();
-        System.out.println("Current date is " + d1);
-        //Date d2 = new Date(2323223232L);
-        //System.out.println("Date represented is "+ d2 );
+    private static Scanner scanner = new Scanner(System.in);
+    private static List<Ticket> TicketArray = new ArrayList<Ticket>();
+    private static Ticket ticket1 = new Ticket("John Doe", 33, "abc@gmail.com","male","123-123-1234", "01/01/2022","IAD", "FRN");
+    private static Ticket tempTicket = new Ticket();
 
-        Date d3 = new Date(2022,01, 01);
-        System.out.println(d3);
+    public static void main(String[] args) throws ParseException {
+//        Date d1 = new Date();
+//        System.out.println("Current date is " + d1);
+//        //Date d2 = new Date(2323223232L);
+//        //System.out.println("Date represented is "+ d2 );
+//
+//        Date d3 = new Date(2022,01, 01);
+//        System.out.println(d3);
 
         String sDate1="31/12/1998";
         Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1);
         System.out.println(sDate1+"\t"+date1);
 
+    menuEntry();
         
     }
-}
+
+    public static void menuEntry() {
+        System.out.println("""
+                \n======================================================================
+                Welcome to ACME Airways. You are going on a trip.  This application
+                will assits you in getting you a boarding pass / ticket.  Please read 
+                the prompts carefully to get you through this process as quickly a possible.
+                ========================================================================
+                """);
+
+        while (true) {
+            switch (pickMenuEntry()) {
+                case 1:
+                    System.out.println("You selected to Book a Flight / Create a Ticket...");
+                    createTicket();
+                    break;
+                case 2:
+                    System.out.println("You selected to Delete a Ticket");
+                    //register();
+                    //foodMenu();
+                    break;
+                case 3:
+                    System.out.println("You selected to Display Tickets Info...");
+//                    for (Ticket tickt: TicketArray) {
+//                        System.out.println("Index: " + TicketArray.indexOf(waiter));
+//                        System.out.println("Name: " + ticket.getFirstName());
+//                        System.out.println("Last Name: " + waiter.getLastName());
+//                        System.out.println("Phone No: " + waiter.getPhoneNo());
+//                        System.out.println("Employee ID: " + waiter.getEmployeeID());
+//                        System.out.println("Shift: " + waiter.getShift());
+//                        System.out.println();
+            //          }
+
+                    break;
+                case 4:
+                    System.out.println("You selected to Exit...");
+                    System.exit(0);
+                default:
+                    System.out.println("Incorrect Entry, please select between 1-4");
+            }
+        }
+    }
+
+        public static int pickMenuEntry() {
+            System.out.println("""
+                =======================
+                1- Book a Flight / Create a Ticket
+                2- Delete Existing Ticket
+                3- Display Tickets Info
+                4- Exit
+                =======================
+                    """);
+            return scanner.nextInt();
+        }
+
+        public static void createTicket() {
+        //add test ticket1
+        TicketArray.add(ticket1);
+        System.out.println(ticket1);
+
+        //add temp Ticket
+        tempTicket.addTicket();
+        TicketArray.add(tempTicket);
+
+        System.out.println(TicketArray);
+
+        }
+
+
+    }
+
--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ticket {
     //Fields
     String passengerName;
@@ -6,14 +8,174 @@ public class Ticket {
     String passengerGender;
     String passengerPhoneNo;
     String departureDate;
-    int boardingPassNo;
     String originAirport;
     String destinationAirport;
+    ///
+    String ETA;
+    int price;
+    int boardingPassNo;
+
 
     //Constructors
 
+    public Ticket() {
+    }
+
+    public Ticket(String passengerName, int passengerAge, String passengerEmail, String passengerGender, String passengerPhoneNo, String departureDate, String originAirport, String destinationAirport) {
+        this.passengerName = passengerName;
+        this.passengerAge = passengerAge;
+        this.passengerEmail = passengerEmail;
+        this.passengerGender = passengerGender;
+        this.passengerPhoneNo = passengerPhoneNo;
+        this.departureDate = departureDate;
+        this.originAirport = originAirport;
+        this.destinationAirport = destinationAirport;
+    }
+
+    public Ticket(String passengerName, int passengerAge, String passengerEmail, String passengerGender, String passengerPhoneNo, String departureDate, String originAirport, String destinationAirport, String ETA, int price, int boardingPassNo) {
+        this.passengerName = passengerName;
+        this.passengerAge = passengerAge;
+        this.passengerEmail = passengerEmail;
+        this.passengerGender = passengerGender;
+        this.passengerPhoneNo = passengerPhoneNo;
+        this.departureDate = departureDate;
+        this.originAirport = originAirport;
+        this.destinationAirport = destinationAirport;
+        this.ETA = ETA;
+        this.price = price;
+        this.boardingPassNo = boardingPassNo;
+    }
+
     // Getters and Setters
+    public String getPassengerName() {
+        return passengerName;
+    }
+
+    public void setPassengerName(String passengerName) {
+        this.passengerName = passengerName;
+    }
+
+    public int getPassengerAge() {
+        return passengerAge;
+    }
+
+    public void setPassengerAge(int passengerAge) {
+        this.passengerAge = passengerAge;
+    }
+
+    public String getPassengerEmail() {
+        return passengerEmail;
+    }
+
+    public void setPassengerEmail(String passengerEmail) {
+        this.passengerEmail = passengerEmail;
+    }
+
+    public String getPassengerGender() {
+        return passengerGender;
+    }
+
+    public void setPassengerGender(String passengerGender) {
+        this.passengerGender = passengerGender;
+    }
+
+    public String getPassengerPhoneNo() {
+        return passengerPhoneNo;
+    }
+
+    public void setPassengerPhoneNo(String passengerPhoneNo) {
+        this.passengerPhoneNo = passengerPhoneNo;
+    }
+
+    public String getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(String departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public String getOriginAirport() {
+        return originAirport;
+    }
+
+    public void setOriginAirport(String originAirport) {
+        this.originAirport = originAirport;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public void setDestinationAirport(String destinationAirport) {
+        this.destinationAirport = destinationAirport;
+    }
+
+    public String getETA() {
+        return ETA;
+    }
+
+    public void setETA(String ETA) {
+        this.ETA = ETA;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getBoardingPassNo() {
+        return boardingPassNo;
+    }
+
+    public void setBoardingPassNo(int boardingPassNo) {
+        this.boardingPassNo = boardingPassNo;
+    }
 
     //To String
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "passengerName='" + passengerName + '\'' +
+                ", passengerAge=" + passengerAge +
+                ", passengerEmail='" + passengerEmail + '\'' +
+                ", passengerGender='" + passengerGender + '\'' +
+                ", passengerPhoneNo='" + passengerPhoneNo + '\'' +
+                ", departureDate='" + departureDate + '\'' +
+                ", originAirport='" + originAirport + '\'' +
+                ", destinationAirport='" + destinationAirport + '\'' +
+                ", ETA='" + ETA + '\'' +
+                ", price=" + price +
+                ", boardingPassNo=" + boardingPassNo +
+                '}';
+    }
+
+    //Methods
+    public void addTicket() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Passenger Name: ");
+        this.setPassengerName(scanner.nextLine());
+        System.out.print("Passanger Age: ");
+        this.setPassengerAge(scanner.nextInt());
+        scanner.nextLine();
+        System.out.print("Passanger Email: ");
+        this.setPassengerEmail(scanner.nextLine());
+        System.out.print("Passenger Gender: ");
+        this.setPassengerGender(scanner.nextLine());
+        System.out.print("Phone No: ");
+        this.setPassengerPhoneNo(scanner.nextLine());
+        System.out.print("Departure Date: ");
+        this.setDepartureDate(scanner.nextLine());
+        System.out.print("Origin Airport: ");
+        this.setOriginAirport(scanner.nextLine());
+        System.out.print("Destination Airport: ");
+        this.setOriginAirport(scanner.nextLine());
+
+        //this.shift = scanner.nextLine();
+    }
+
 
 }
